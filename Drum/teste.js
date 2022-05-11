07c5d723ac582a1e4aeb0a32ea8de9b91e1a2bd0
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2022-05-10 23:13:35 -0300

    Add files via upload

---
+++ Drum/teste.js
@@ -0,0 +1,15 @@
+var atendente1 = {
+    nome: "antonio",
+    idade: "19",
+    função: ["Caixa","Estoque"],
+
+}
+
+class atendentes {
+    constructor(nome, idade, função) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.função = função;
+    }
+}
